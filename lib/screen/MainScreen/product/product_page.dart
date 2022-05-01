fee532a217e80ab1716c84628b8ca7b8edf53861
Danu Adi Nugroho <gaul.gaul5@gmail.com>
2022-05-01 14:52:57 +0700

    Added admin product page

---
+++ lib/screen/MainScreen/product/product_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:java_ijen_mobile/screen/MainScreen/product/produk.dart';
+import 'package:java_ijen_mobile/screen/MainScreen/product/produkDB.dart';
+import '../../../utils/auth.dart';
+import '../../../const.dart';
+
+class ProductPage extends StatefulWidget {
+  UserData userData;
+
+  ProductPage({Key? key, required this.userData}) : super(key: key);
+
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  bool _isLoading = false;
+  late List<Produk> _listProduk;
+  late int _totalData;
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
+  void fetchData() async {
+    setState(() {
+      _isLoading = true;
+    });
+    _listProduk = await ProdukDB().getProduk();
+    _totalData = _listProduk.length;
+    print(_listProduk[0].img);
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Kalo mau akses role bisa pake widget.userData.role disini
+
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+        body: SafeArea(
+      child: (widget.userData.role == "admin")
+          ? AdminView(screenSize, context)
+          : PembeliView(screenSize),
+    ));
+  }
+
+  Widget PembeliView(Size screenSize) {
+    return Container(
+      child: Text(widget.userData.role),
+    );
+  }
+
+  Widget AdminView(Size screenSize, BuildContext context) {
+    return Container(
+        child: (_isLoading)
+            ? Center(child: const CircularProgressIndicator())
+            : ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: _totalData,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                      onTap: () {
+                        print("card");
+                      },
+                      child: Card(
+                        child: Column(children: [
+                          Container(
+                              height: 150.0,
+                              child: Ink.image(
+                                image: NetworkImage(_listProduk[index].img),
+                                fit: BoxFit.cover,
+                              )),
+                          ListTile(
+                            title: Text(_listProduk[index].nama),
+                            subtitle: Text(
+                                "Rp${_listProduk[index].harga}/kg - Stok ${_listProduk[index].jumlah} kg"),
+                            trailing: Wrap(spacing: 12, children: [
+                              IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {
+                                  print("edit");
+                                },
+                              ),
+                            ]),
+                          )
+                        ]),
+                      ));
+                },
+              ));
+  }
+}
---
+++ lib/screen/MainScreen/product/produkDB.dart
@@ -0,0 +1,118 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:java_ijen_mobile/screen/MainScreen/product/produk.dart';
+
+class ProdukDB {
+  FirebaseDatabase db = FirebaseDatabase.instance;
+
+  Future<void> addLahan(
+      String alamat, String pemilik, String lat, String long) async {
+    DatabaseReference ref = db.ref('produk');
+    await ref
+        //.child()
+        .set({"alamat": alamat, "pemilik": pemilik, "lat": lat, "long": long});
+  }
+
+  Future<List<Produk>> getProduk() async {
+    final List<Produk> produk = [];
+    DatabaseReference ref = FirebaseDatabase.instance.ref('produk');
+
+    final root = await ref.get();
+    for (final id in await root.children) {
+      String nama = "";
+      String harga = "";
+      String jumlah = "";
+      String petani = "";
+      String lahan = "";
+      String proses = "";
+      String pid = id.key.toString();
+      String img = "";
+
+      final firstChild = await ref.child(pid).get();
+      for (final col in firstChild.children) {
+        if (col.key == "harga") {
+          harga = col.value.toString();
+        }
+        if (col.key == "jumlah") {
+          jumlah = col.value.toString();
+        }
+        if (col.key == "lahan") {
+          lahan = col.value.toString();
+        }
+        if (col.key == "nama") {
+          nama = col.value.toString();
+        }
+        if (col.key == "petani") {
+          petani = col.value.toString();
+        }
+        if (col.key == "proses") {
+          proses = col.value.toString();
+        }
+        if (col.key == "img") {
+          img = col.value.toString();
+        }
+        // if (col.key == "pemilik") {
+        //   idpemilik = col.value.toString();
+        //   DatabaseReference refPT = db.ref('petani');
+        //   final petaniChild =
+        //   await refPT.child(col.value.toString()).child("nama").get();
+        //   namapemilik = petaniChild.value.toString();
+        // }
+      }
+      produk.insert(produk.length,
+          Produk(pid, nama, jumlah, petani, lahan, proses, harga, img));
+    }
+    return produk;
+  }
+
+  Future<Produk> getDataById(String id) async {
+    DatabaseReference ref = db.ref('produk/$id');
+    String nama = "";
+    String harga = "";
+    String jumlah = "";
+    String petani = "";
+    String lahan = "";
+    String proses = "";
+    String img = "";
+
+    final firstChild = await ref.get();
+    for (final col in firstChild.children) {
+      if (col.key == "harga") {
+        harga = col.value.toString();
+      }
+      if (col.key == "jumlah") {
+        jumlah = col.value.toString();
+      }
+      if (col.key == "lahan") {
+        lahan = col.value.toString();
+      }
+      if (col.key == "nama") {
+        nama = col.value.toString();
+      }
+      if (col.key == "petani") {
+        petani = col.value.toString();
+      }
+      if (col.key == "proses") {
+        proses = col.value.toString();
+      }
+      if (col.key == "img") {
+        img = col.value.toString();
+      }
+    }
+    Produk produkItem =
+        Produk(id, nama, jumlah, petani, lahan, proses, harga, img);
+    return produkItem;
+  }
+
+  Future<void> updateProduk(String id, String nama, String jumlah,
+      String petani, String lahan, String proses, String harga) async {
+    DatabaseReference ref = db.ref('produk/$id');
+    await ref.update({
+      "nama": nama,
+      "jumlah": jumlah,
+      "petani": petani,
+      "lahan": lahan,
+      "proses": proses,
+      "harga": harga
+    });
+  }
+}
