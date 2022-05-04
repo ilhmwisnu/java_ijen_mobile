11d4bfd19110e16368edc41ed36f5a31ff2203bc
ilhmwisnu <ilhamwisnu42@gmail.com>
2022-05-05 00:26:57 +0700

    edited product page view

--- lib/screen/MainScreen/product/product_page.dart
+++ lib/screen/MainScreen/product/product_page.dart
@@ -49,6 +49,10 @@ class _ProductPageState extends State<ProductPage> {
     final screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
+        appBar: AppBar(
+          title: Text("PRODUK"),
+          backgroundColor: darkGrey,
+        ),
         floatingActionButton: (widget.userData.role == "admin")
             ? FloatingActionButton(
                 onPressed: () {
@@ -60,79 +64,105 @@ class _ProductPageState extends State<ProductPage> {
               )
             : null,
         body: SafeArea(
-          child: (widget.userData.role == "admin")
-              ? AdminView(screenSize, context)
-              : PembeliView(screenSize),
+          child: AdminView(screenSize, context),
         ));
   }
 
-  Widget PembeliView(Size screenSize) {
-    return Container(
-        child: (_isLoading)
-            ? Center(child: const CircularProgressIndicator())
-            : ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: _totalData,
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                      onTap: () {
-                        print("card");
-                      },
-                      child: Card(
-                        child: Column(children: [
-                          Container(
-                              height: 150.0,
-                              child: Ink.image(
-                                image: NetworkImage(_prodImg[index]),
-                                fit: BoxFit.cover,
-                              )),
-                          ListTile(
-                            title: Text(_listProduk[index].nama),
-                            subtitle: Text(
-                                "Rp${_listProduk[index].harga}/kg - Stok ${_listProduk[index].jumlah} kg"),
-                          )
-                        ]),
-                      ));
-                },
-              ));
-  }
+  // Widget PembeliView(Size screenSize) {
+  //   return Container(
+  //       child: (_isLoading)
+  //           ? Center(child: const CircularProgressIndicator())
+  //           : ListView.builder(
+  //               padding: const EdgeInsets.all(8),
+  //               itemCount: _totalData,
+  //               itemBuilder: (BuildContext context, int index) {
+  //                 return GestureDetector(
+  //                     onTap: () {
+  //                       print("card");
+  //                     },
+  //                     child: Card(
+  //                       child: Column(children: [
+  //                         Container(
+  //                             height: 150.0,
+  //                             child: Ink.image(
+  //                               image: NetworkImage(_prodImg[index]),
+  //                               fit: BoxFit.cover,
+  //                             )),
+  //                         ListTile(
+  //                           title: Text(_listProduk[index].nama),
+  //                           subtitle: Text(
+  //                               "Rp${_listProduk[index].harga}/kg - Stok ${_listProduk[index].jumlah} kg"),
+  //                         )
+  //                       ]),
+  //                     ));
+  //               },
+  //             ));
+  // }
 
   Widget AdminView(Size screenSize, BuildContext context) {
     return Container(
-        child: (_isLoading)
-            ? Center(child: const CircularProgressIndicator())
-            : ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: _totalData,
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                      onTap: () {
-                        print("card");
-                      },
-                      child: Card(
-                        child: Column(children: [
-                          Container(
-                              height: 150.0,
-                              child: Ink.image(
-                                image: NetworkImage(_prodImg[index]),
+      child: (_isLoading)
+          ? Center(child: const CircularProgressIndicator())
+          : GridView.builder(
+              itemCount: _totalData,
+              padding: EdgeInsets.all(defaultPadding),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 3 / 3.9,
+                crossAxisSpacing: defaultPadding,
+                mainAxisSpacing: defaultPadding,
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                    child: Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 150,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
                                 fit: BoxFit.cover,
-                              )),
-                          ListTile(
-                            title: Text(_listProduk[index].nama),
-                            subtitle: Text(
-                                "Rp${_listProduk[index].harga}/kg - Stok ${_listProduk[index].jumlah} kg"),
-                            trailing: Wrap(spacing: 12, children: [
-                              IconButton(
-                                icon: Icon(Icons.edit),
-                                onPressed: () {
-                                  print("edit");
-                                },
+                                image: NetworkImage(_prodImg[index]))),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _listProduk[index].nama,
+                                style: TextStyle(fontSize: 16),
                               ),
-                            ]),
-                          )
-                        ]),
-                      ));
-                },
-              ));
+                              Text("Rp ${_listProduk[index].harga} /kg",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600)),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Stok : ${_listProduk[index].jumlah}",
+                                    style:
+                                        TextStyle(color: Colors.grey.shade600),
+                                  ),
+                                  (widget.userData.role == "admin")
+                                      ? IconButton(
+                                          color: Colors.grey.shade500,
+                                          padding: EdgeInsets.zero,
+                                          onPressed: () {},
+                                          constraints: BoxConstraints(),
+                                          icon: Icon(Icons.edit))
+                                      : Container()
+                                ],
+                              )
+                            ],
+                          ))
+                    ],
+                  ),
+                ));
+              }),
+    );
   }
 }
